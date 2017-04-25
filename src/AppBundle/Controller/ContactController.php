72e9bd26a6b92fae4fd68a97008fe5ff9473f0ea
Marek Bazylewicz <mbazyl2@gmail.com>
2017-04-25 19:51:01 +0200

    New action with form view. New action for adding contact to database.

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Contact;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ContactController extends Controller
@@ -16,5 +19,36 @@ class ContactController extends Controller
     public function indexAction()
     {
         return new Response("[]");
+    }
+
+    /**
+     * @Route("/new")
+     * @Template(":contact:new_form.html.twig")
+     * @Method("GET")
+     */
+    public function newContactAction()          // akcja przekierowujaca na widok formularza
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/create")
+     * @Template("::base.html.twig")
+     * @Method("POST")
+     */
+    public function createContactAction(Request $request)           // akcja tworzaca nowy obiekt na podstawie danych z formularza
+    {
+        $contact = new Contact();                                   // nowy obiekt $contact klasy Contact
+        $contact->setName($request->request->get('name'));
+        $contact->setSurname($request->request->get("surname"));
+        $contact->setNickname($request->request->get("nickname"));
+        // nadanie nowemu obiektowi za pomoca setterow atrybutow przeslanych formularzem
+
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($contact);
+        $em->flush();
+        return [];
+        // narazie nie jest zwracane nic, ale akcja ta, po dodaniu obiektu do bazy bedzie przekierowywac do wyswietlenia dodanego kontaktu lub wszystkich kontaktow
     }
 }
